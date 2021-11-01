3df81d9e7bcaf716cbc3d4e26cbcd3ec0a47ecda
Saymon Victor <sayminnv88@gmail.com>
2021-11-01 16:40:50 -0300

    Adicionando funcionalidades

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 //Carne == 400g por pessoa --> + de 6h == 650
 //Cerveja == 1200ml por pessoa --> + 6h == 2000ml
 //Refri/Água == 1000ml por pessoa --> + 6h == 1500ml
+
+function Calcular() {
+  let qntAdultos = document.querySelector('#qntAdultos')
+  let qntCriancas = document.querySelector('#qntCriancas')
+  let numeroHoras = document.querySelector('#numeroHoras')
+
+  let carne
+  let cerveja
+  let agua
+
+  if (numeroHoras.value.length == 0) {
+    alert('Digite a quantidade de horas')
+  } else {
+    if (numeroHoras.value >= 6) {
+      carne = 650 + qntAdultos.value + (650 / 2 + qntCriancas.value)
+      cerveja = 2000 + qntAdultos.value
+      agua = 1500 + qntAdultos.value + (1500 / 2 + qntCriancas.value)
+    } else {
+      carne = 400 + qntAdultos.value + (400 / 2 + qntCriancas.value)
+      cerveja = 1200 + qntAdultos.value
+      agua = 1000 + qntAdultos.value + (1000 / 2 + qntCriancas.value)
+    }
+  }
+}
