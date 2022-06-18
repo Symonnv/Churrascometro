749061d8f226d1f113a3f4e4d6b8b46875e60c5b
Saymon Victor <sayminnv88@gmail.com>
2022-06-18 10:30:01 -0300

    starting refactoring

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-let qntAdultos = document.querySelector('#qntAdultos')
-let qntCriancas = document.querySelector('#qntCriancas')
-let numeroHoras = document.querySelector('#numeroHoras')
-let resultado = document.querySelector('#resultado')
+let qntAdultos = document.querySelector("#qntAdultos");
+let qntCriancas = document.querySelector("#qntCriancas");
+let numeroHoras = document.querySelector("#numeroHoras");
+let resultado = document.querySelector("#resultado");
 
 function Calcular() {
-  let adultos = qntAdultos.value
-  let criancas = qntCriancas.value
-  let duracao = numeroHoras.value
+  let adultos = qntAdultos.value;
+  let criancas = qntCriancas.value;
+  let duracao = numeroHoras.value;
 
   if (duracao.length == 0) {
-    alert('Digite a quantidade de horas')
+    alert("Digite a quantidade de horas");
   } else {
-    let qntCarne = carne(duracao) * adultos + (carne(duracao) / 2) * criancas
-    let qntCerveja = cerveja(duracao) * adultos
-    let qntAgua = agua(duracao) * adultos + (agua(duracao) / 2) * criancas
+    let qntCarne = carne(duracao) * adultos + (carne(duracao) / 2) * criancas;
+    let qntCerveja = cerveja(duracao) * adultos;
+    let qntAgua = agua(duracao) * adultos + (agua(duracao) / 2) * criancas;
 
-    resultado.style.cssText = 'margin-top:10px'
-    resultado.innerHTML = `<p>${qntCarne / 1000}Kg de carne</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qntCerveja / 355)} latas de cerveja</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qntAgua / 2000)} garrafas de água</p>`
+    resultado.style.cssText = "margin-top:10px";
+    resultado.innerHTML = `<p>${qntCarne / 1000}Kg de carne</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(
+      qntCerveja / 355
+    )} latas de cerveja</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(
+      qntAgua / 2000
+    )} garrafas de água</p>`;
   }
 }
 function carne(duracao) {
   if (duracao >= 6) {
-    return 650
+    return 650;
   } else {
-    return 400
+    return 400;
   }
 }
 function cerveja(duracao) {
   if (duracao >= 6) {
-    return 2000
+    return 2000;
   } else {
-    return 1200
+    return 1200;
   }
 }
 function agua(duracao) {
   if (duracao >= 6) {
-    return 1500
+    return 1500;
   } else {
-    return 1000
+    return 1000;
   }
 }
